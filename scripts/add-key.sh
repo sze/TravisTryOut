c230a88fae1ecf7dbf289d354a2451ef349e09f2
Larissa <lk@tailored-apps.com>
2013-12-27 15:31:56 +0100

    changed the encryption and the file names...the names make more sense now...the tutorial has some mistakes

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -2,6 +2,6 @@
 security create-keychain -p travis ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "$KEYCHAINPWD" -T /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEYCHAINPWD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
