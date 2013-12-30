a5679a7ec1990bbb3d2164f8789d7f6689b361f9
Larissa <lk@tailored-apps.com>
2013-12-30 19:10:55 +0100

    another try to build the ipa-file

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -11,6 +11,7 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
+ls "$HOME/Library/MobileDevice/Provisioning Profiles/"
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
 
