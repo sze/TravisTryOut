2f9c52bde80cd9b91050e35f84a1d5c466e9d8ec
Hannes Satz <hannes@tailored-apps.com>
2014-01-06 13:03:00 +0100

    fixed the ignore and the plist generation

--- scripts/sign_deploy_hockey.sh
+++ scripts/sign_deploy_hockey.sh
@@ -14,7 +14,7 @@ OUTPUTDIR="$PWD/build/Release-iphoneos"
 ls "$HOME/Library/MobileDevice/Provisioning Profiles/"
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
 
-cat "$PWD/TravisTryOut/scripts/Template.plist" | sed -e "s/\##BUNDLEID##/$BUNDLE_IDENTIFIER/" -e "s/\##TITLE##/$APPNAME/" > "$OUTPUTDIR/$APPNAME.plist"
+cat "$PWD/scripts/Template.plist" | sed -e "s/\##BUNDLEID##/$BUNDLE_IDENTIFIER/" -e "s/\##TITLE##/$APPNAME/" > "$OUTPUTDIR/$APPNAME.plist"
   
   
 curl --ftp-create-dirs -T "$OUTPUTDIR/$APPNAME.plist" -u $TABETAUSR:$TABETAPWD "ftp://$TABETAURL/$APPNAME/"
