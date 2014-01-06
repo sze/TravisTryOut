a6ef1e76b22d73d7a7097ac298b097fc32394e2b
Hannes Satz <hannes@tailored-apps.com>
2014-01-06 12:23:13 +0100

    added hockey deployment and the manual creation of the plist file

---
+++ scripts/sign_deploy_hockey.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Testing on a branch other than master. No deployment will be done."
+  exit 0
+fi
+
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+OUTPUTDIR="$PWD/build/Release-iphoneos"
+
+ls "$HOME/Library/MobileDevice/Provisioning Profiles/"
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+cat "$PWD/TravisTryOut/scripts/Template.plist" | sed -e "s/\##BUNDLEID##/$BUNDLE_IDENTIFIER/" -e "s/\##TITLE##/$APPNAME/" > "$OUTPUTDIR/$APPNAME.plist"
+  
+  
+curl --ftp-create-dirs -T "$OUTPUTDIR/$APPNAME.plist" -u $BETAUSR:$BETAPWD "ftp://$BETAURL/$APPNAME/"
+curl --ftp-create-dirs -T "$OUTPUTDIR/$APPNAME.ipa" -u $BETAUSR:$BETAPWD "ftp://$BETAURL/$APPNAME/"
